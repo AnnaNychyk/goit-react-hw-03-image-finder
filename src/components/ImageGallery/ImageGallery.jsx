8eb1a043ff499f5c23bcf9a2ae9af083a9bfd07b
AnnaNychyk <annanichichka@gmail.com>
2022-07-29 16:48:37 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -8,16 +8,20 @@ class ImageGallery extends Component {
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.word !== this.props.word) {
             console.log('Change enter word')
+            console.log('this.props.word: ', this.props.word);
 
-            fetch(`https://pixabay.com/api/?q=${this.props.word}&page=1&key=25809768-5f151ed3e9c60947c53759114&image_type=photo&orientation=horizontal&per_page=12`).then(response => response.json()).then(word => this.setState({word}));
+
+            fetch(`https://pixabay.com/api/?q=${this.props.word}&page=1&key=25809768-5f151ed3e9c60947c53759114&image_type=photo&orientation=horizontal&per_page=12`).then(response => response.json()).then(
+                // console.log
+                word => this.setState({word})
+            );
         }
     }
 
     render() {
         return (
             <div>
-                <p>WORD</p>
-                {this.state.word && <p>{this.state.word.hits.id}</p>}
+                {this.state.word && <p>{this.state.word.totalHits}</p>}
             </div>
         )
     }
